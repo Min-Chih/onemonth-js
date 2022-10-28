6795c57fb0ef3f9d2a4535febfc544918157c2ca
Min-Chih <min.chih98@gmail.com>
2022-10-28 15:59:21 +0100

    finalised get user input function and fetch Shazam API function

--- week-3/javascript/main.js
+++ week-3/javascript/main.js
@@ -5,23 +5,56 @@ let button = document.querySelector('i');
 
 
 button.addEventListener('click', function(e){
-    let userInput = document.querySelector('.js-search').value;
-    console.log(userInput);
+    let userInput = document.querySelector('input').value;
+    getInput(userInput);
+	console.log(userInput);
 });
+
+// When press Enter, event listener works
+
+document.querySelector('input').addEventListener('keypress', function(e) {
+  
+    var input = document.querySelector("input").value;
+    // if the key ENTER is pressed
+    if(e.key === 'Enter') {
+        getInput(input);
+    }
+})
 
 // Step2: Query Soundcloud API
 
-SC.initialize({
-    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
+// SC.initialize({
+//     client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+// });
 
-// Find alll sounds of buskers 
+// // Find alll sounds of buskers 
 
-SC.get('/tracks', {
-    q: 'buskers'
-}).then(function(tracks) {
-    console.log(tracks);
-});
+// SC.get('/tracks', {
+//     q: 'buskers'
+// }).then(function(tracks) {
+//     console.log(tracks);
+// });
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e030a21df6mshd7affff6fdcf43bp10819ejsnad2fdf4a08d1',
+		'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+	}
+};
+
+
+function getInput(userinput) {
+    var search = userinput.split(' ').join('%20');
+	let url = `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=5`
+	fetch('https://shazam.p.rapidapi.com/search?term=kiss%20the%20rain&locale=en-US&offset=0&limit=5', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+};
+
+
 
 
 // Step3: Display the cards
