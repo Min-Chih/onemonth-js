665518b3d092afef8d5c1821281770cddcdb0cc5
Min-Chih <min.chih98@gmail.com>
2022-10-30 00:01:37 +0100

    changed fetch method, and added for loop to loop through the data

--- week-3/javascript/main.js
+++ week-3/javascript/main.js
@@ -21,19 +21,7 @@ document.querySelector('input').addEventListener('keypress', function(e) {
     }
 })
 
-// Step2: Query Soundcloud API
-
-// SC.initialize({
-//     client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-// });
-
-// // Find alll sounds of buskers 
-
-// SC.get('/tracks', {
-//     q: 'buskers'
-// }).then(function(tracks) {
-//     console.log(tracks);
-// });
+// Step2: Query Shazam API
 
 
 const options = {
@@ -45,69 +33,67 @@ const options = {
 };
 
 
-function getInput(userinput) {
+async function getInput(userinput) {
+
     var search = userinput.split(' ').join('%20');
-	let url = `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=5`
-	fetch(url, options)
-	.then(response => response.json())
-	.then(response => console.log(response.artists))
+	let url = `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=5`;
+	let response = await fetch(url, options);
+	let data = await response.json();
+	console.log(data);
+
+	for(let i=0; i < 6; i++) {
+
+		// card
+		let card = document.createElement('div');
+		card.classList.add('card');
 	
-	.catch(err => console.error(err));
+		// image
+		let imageDiv = document.createElement('div');
+		imageDiv.classList.add('image');
+	
+		let image_img = document.createElement('img');
+		image_img.classList.add('image_img');
+		let imageSrc = data.tracks.hits[i].track.images.coverart;
+		image_img.src = imageSrc;
+	
+		imageDiv.appendChild(image_img);
+	
+		//content 
+		let content = document.createElement('div');
+		content.classList.add('content');
+	
+		let header = document.createElement('div');
+		header.classList.add('header');
+		header.innerHTML = `<a href="#" target="_blank">${data.tracks.hits[i].track.title}</a>`;
+	
+		// button 
+		let addButton = document.createElement('div');
+		addButton.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+	
+		let icon = document.createElement('div');
+		icon.classList.add('add', 'icon');
+	
+		let buttonText = document.createElement('span');
+		buttonText.innerHTML = 'Add to playlist';
+	
+		//appendChild
+		content.appendChild(header);
+	
+		addButton.appendChild(icon);
+		addButton.appendChild(buttonText);
+	
+		card.appendChild(imageDiv);
+		card.appendChild(content);
+		card.appendChild(addButton);
+	
+		let searchResults = document.querySelector('.js-search-results');
+		searchResults.appendChild(card);
+
+
+	}
+		
+
 };
-
-function renderTracks (tracks) {
-
-
-	// card
-	let card = document.createElement('div');
-	card.classList.add('card');
-
-	// image
-	let imageDiv = document.createElement('div');
-	imageDiv.classList.add('image-container');
-
-	let image_img = document.createElement('img');
-	image_img.classList.add('image');
-	//let imageSrc = response.artists.hits[i].artist.avatar
-	image_img.src = 'http://www.placekitten.com/290/290';
-
-	imageDiv.appendChild(image_img);
-
-	//content 
-	let content = document.createElement('div');
-	content.classList.add('content');
-
-	let header = document.createElement('div');
-	header.classList.add('header');
-	header.innerHTML = `<a href="#" target="_blank">Hello</a>`;
-
-	// button 
-	let addButton = document.createElement('div');
-	addButton.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
-
-	let icon = document.createElement('div');
-	icon.classList.add('add', 'icon');
-
-	let buttonText = document.createElement('span');
-	buttonText.innerHTML = 'Add to playlist';
-
-	//appendChild
-	content.appendChild(header);
-
-	addButton.appendChild(icon);
-	addButton.appendChild(buttonText);
-
-	card.appendChild(imageDiv);
-	card.appendChild(content);
-	card.appendChild(addButton);
-
-	let searchResults = document.querySelector('.js-search-results');
-	searchResults.appendChild(card);
-
-}
-
-renderTracks();
-
 
 // Step3: Display the cards
 
